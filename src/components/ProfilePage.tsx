c7008395c35f58086bbe299d4efae185c54401c5
AJ Ross <lasersox@fluxon.com>
2024-06-04 14:18:12 +0300

    Added Profile page.

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,134 +1,40 @@
 import { Box, Button, Flex, Heading, Input, Stack, HStack, Spinner, Text, Avatar } from '@chakra-ui/react';
 import { collection, addDoc, updateDoc, deleteDoc, doc, query, CollectionReference } from 'firebase/firestore';
-import { useState } from 'react';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+import { Auth, getAuth } from 'firebase/auth';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { Navigate } from 'react-router-dom';
 
 import { db } from '../../firebase.config';
+
+const auth = getAuth();
 
 interface User {
   id: string;
   name: string;
   mark: number;
 }
-export function FirebaseDemo() {
-  // const [newName, setNewName] = useState('');
-  // const [newMark, setNewMark] = useState(0);
 
-  // const usersCollectionRef = collection(db, 'users');
-
-  // const [users, usersLoading, usersError] = useCollection(query(usersCollectionRef) as CollectionReference<User>);
-
-  // const createUser = async () => {
-  //   await addDoc(usersCollectionRef, { name: newName, mark: Number(newMark) });
-  // };
-
-  // const updateUser = async (id: string, mark: number) => {
-  //   const userDoc = doc(db, 'users', id);
-  //   const newFields = { mark: mark + 1 };
-  //   await updateDoc(userDoc, newFields);
-  // };
-
-  // const deleteUser = async (id: string) => {
-  //   const userDoc = doc(db, 'users', id);
-  //   await deleteDoc(userDoc);
-  // };
-
-  // if (usersLoading) {
-  //   return <Spinner />;
-  // }
-
-  // if (usersError) {
-  //   return <Box>Error fetching users</Box>;
-  // }
-
-  // return (
-  //   <Flex flexDir="column" gap="8" padding="6">
-  //     <Flex flexDir="column" gap="6" border="1px" borderColor="gray.200" width="20%" px="6" py="8">
-  //       <Stack spacing="3">
-  //         <Input
-  //           onChange={(event) => {
-  //             setNewName(event.target.value);
-  //           }}
-  //           placeholder="Name..."
-  //           size="sm"
-  //         />
-  //         <Input
-  //           type="number"
-  //           placeholder="Mark..."
-  //           onChange={(event) => {
-  //             setNewMark(Number(event.target.value));
-  //           }}
-  //           size="sm"
-  //         />
-  //       </Stack>
-  //       <Button width="50%" size="sm" colorScheme="green" onClick={createUser}>
-  //         Create User
-  //       </Button>
-  //     </Flex>
-  //     <Flex gap="4" flexWrap="wrap">
-  //       {users &&
-  //         users.docs.map((user) => (
-  //           <Box gap="6" border="1px" borderColor="gray.300" width="20%" px="6" py="8" key={user.id}>
-  //             <Heading>Name: {user.data().name}</Heading>
-  //             <Heading>Mark: {user.data().mark}</Heading>
-  //             <HStack gap="4" mt="4">
-  //               <Button size="sm" colorScheme="green" onClick={() => updateUser(user.id, user.data().mark)}>
-  //                 Increase Mark
-  //               </Button>
-  //               <Button size="sm" colorScheme="red" onClick={() => deleteUser(user.id)}>
-  //                 Delete User
-  //               </Button>
-  //             </HStack>
-  //           </Box>
-  //         ))}
-  //     </Flex>
-  //   </Flex>
-  // );
-
-  return (
-    <Flex flexDir="column" gap="8" padding="6">
-      <Flex flexDir="column" gap="8" padding="6">
-        <Flex flexDir="row" gap="8" padding="6">
-          <Box flex='1' bg='tomato'>
-            <Text>Personal info</Text>
-          </Box>
-          <Box flex='1' bg='tomato'>
-            <Text>edit</Text>
-          </Box>
-        </Flex>
-        <Flex flexDir="row" gap="8" padding="6">
-          <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
-          <Box>Name</Box>
-          <Box>5 posts</Box>
-        </Flex>
-      </Flex>
-      <Flex flexDir="column" gap="8" padding="6">
-        <Flex flexDir="row" gap="8" padding="6">
-          Photos
-        </Flex>
-        <Flex flexDir="row" gap="8" padding="6">
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-          <Box bg='tomato'>
-            <Text>Image 1</Text>
-          </Box>
-        </Flex>
-      </Flex>
-    </Flex>
+export function ProfilePage() {
+  const [user, userLoading] = useAuthState(auth);
+  const [signOut, isSigningOut] = useSignOut(auth);
+  const usersCollectionRef = collection(db, 'users');
+  const [userProfile, loadingUserProfile, errorLoadingUserProfile] = useDocument(
+    doc(usersCollectionRef, user?.uid || 'asdf')
   );
+  console.log(userProfile?.data());
+
+  // Do not show page content until auth state is fetched.
+  if (userLoading) {
+    return <Spinner />;
+  }
+
+  // If user isn't signed in, redirect to auth page.
+  if (!user) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  return <Box>{userProfile?.data()?.email} {userProfile?.data()?.name}</Box>;
 }
 
-export default FirebaseDemo;
+export default ProfilePage;
