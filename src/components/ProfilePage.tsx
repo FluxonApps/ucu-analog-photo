56335cea63636198f8b49bf42049bec5bfed0cfe
Bazylevych Sofiia <bazylevych.pn@ucu.edu.ua>
2024-06-05 11:29:39 +0300

    seperate code to ProfileCard and ProfileForm

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -13,6 +13,8 @@ import { Navigate } from 'react-router-dom';
 import { db, storage } from '../../firebase.config';
 console.log(storage);
 import { useState } from 'react';
+import { ProfileCard } from './util/ProfileCard';
+import { ProfileForm } from './util/ProfileForm';
 
 const auth = getAuth();
 
@@ -30,7 +32,7 @@ export function ProfilePage() {
     doc(usersCollectionRef, user?.uid || 'asdf')
   );
 
-  const [isEditing, setEditing] = useState(true);
+  const [isEditing, setEditing] = useState(false);
 
   const [newName, setNewName] = useState('');
   const [newProfilePicture, setNewProfilePicture] = useState('');
@@ -40,6 +42,7 @@ export function ProfilePage() {
     const userDoc = doc(db, 'users', id);
     console.log(newFields);
     await updateDoc(userDoc, newFields);
+    setEditing(false)
   };
 
   const addPhoto = async (id: string, photoUrl: string) => {
@@ -48,7 +51,7 @@ export function ProfilePage() {
   }
 
   // Do not show page content until auth state is fetched.
-  if (userLoading) {
+  if (userLoading || loadingUserProfile) {
     return <Spinner />;
   }
 
@@ -58,127 +61,9 @@ export function ProfilePage() {
   }
 
   if (isEditing) {
-    return  <Box><Stack spacing="3">
-    <Input
-      onChange={async (event) => {
-        if (event.target.files && event.target.files.length > 0 && event.target.files[0] !== undefined) {
-          const imageRef = ref(storage, `profilePhotos/${user?.uid}`);
-          console.log(imageRef);
-          const imageSnapshot = await uploadBytes(imageRef, event.target.files[0]);
-          console.log(imageSnapshot);
-          const imageUrl = await getDownloadURL(imageSnapshot.ref);
-          console.log(imageUrl);
-          setNewProfilePicture(imageUrl);
-        }
-      }}
-      accept="image/png,image/jpeg"
-      type="file"
-      size="sm"
-    />
-    <Input
-      defaultValue={userProfile?.data()?.name}
-      onChange={(event) => {
-        setNewName(event.target.value);
-      }}
-      placeholder="Name..."
-      size="sm"
-    />
-    <Input
-      defaultValue={userProfile?.data()?.profile_picture}
-      onChange={(event) => {
-        setNewProfilePicture(event.target.value);
-      }}
-      placeholder="Write yours url image..."
-      size="sm"
-    />
-    <RadioGroup
-      defaultValue={userProfile?.data()?.role}
-      onChange={(value) => {
-        setNewRole(value);
-      }}
-    >
-      <Stack direction='row'>
-        <Radio value='Student'>Student</Radio>
-        <Radio value='Mentor'>Mentor</Radio>
-      </Stack>
-    </RadioGroup>
-  </Stack>
-  <Button width="50%" size="sm" colorScheme="green" onClick={() => {
-      updateUser(user.uid, { name: newName, profile_picture: newProfilePicture, role: newRole });
-      setEditing(false);
-    }}>
-    Update User
-  </Button></Box>
-
+    return  userProfile && <ProfileForm userUid={user.uid} userData={userProfile?.data()} updateUser={updateUser}></ProfileForm>
   } else {
-    return <><Stack justify="spaceAround" px={40} py={10} spacing='15'><>
-    <Wrap>
-    <Input placeholder='Search your photo' width="70vh" bg='#bee8f0'/>
-    <Spacer />
-    <Box>
-    <Avatar name={userProfile?.data()?.name} src={userProfile?.data()?.profile_picture} width="40px" height="40px" />
-    </Box></Wrap>
-    
-    
-    <Flex>
-        <Box>
-          <Heading>Personal info</Heading>
-        </Box>
-        <Spacer />
-        <Button onClick={() => setEditing(true)} bg='#62DAF2' borderRadius='50px'>Edit</Button>
-      </Flex><Flex>
-      <HStack spacing='60px' justify='center'>
-          
-          <Box>
-            <Avatar name={userProfile?.data()?.name} src={userProfile?.data()?.profile_picture} width="100px" height="100px" />
-          </Box>
-          
-          <Box>
-            <Heading>{userProfile?.data()?.name}</Heading>
-            <Text>{userProfile?.data()?.email}</Text>
-            {userProfile?.data()?.role && (
-            <Box as='button' borderRadius='md' bg='#bee8f0' color='gray' px={3} h={6}>
-              {userProfile?.data()?.role}
-            </Box>
-          )} 
-        </Box> 
-
-        <Stack>
-            <Text>3</Text>
-            <Text>Posts</Text>
-          </Stack>
-          </HStack>
-          
-        </Flex></><Flex>
-          <Box>
-            <Heading>Photos</Heading>
-          </Box>
-        </Flex><Flex justifyContent='center'>
-        <Button
-            height='300px'
-            width='300px'
-            border='2px'
-            borderColor='blue.500'
-            borderRadius='0px'
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-          </svg>
-          </Button>
-          <Spacer />
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-          <Spacer />
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-          <Spacer />
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-        </Flex>
-        </Stack></>
+    return <ProfileCard userData={userProfile?.data()} setEditing={setEditing}></ProfileCard>
   }
 }
 
---
+++ src/components/util/ProfileCard.tsx
@@ -0,0 +1,76 @@
+import { Box, Button, Image, Flex, Heading, Input, Stack, HStack, Spinner, Text, Avatar, RadioGroup, Radio, Wrap, WrapItem, Center, Spacer, InputLeftElement, InputGroup} from '@chakra-ui/react';
+
+
+export const ProfileCard =(props: { userData: any; setEditing: any; }) => {
+    console.log(props)
+const { userData, setEditing } = props;
+return <><Stack justify="spaceAround" px={40} py={10} spacing='15'><>
+<Wrap>
+<Input placeholder='Search your photo' width="70vh" bg='#bee8f0'/>
+<Spacer />
+<Box>
+<Avatar name={userData.name} src={userData.profile_picture} width="40px" height="40px" />
+</Box></Wrap>
+
+
+<Flex>
+    <Box>
+      <Heading>Personal info</Heading>
+    </Box>
+    <Spacer />
+    <Button onClick={() => setEditing(true)} bg='#62DAF2' borderRadius='50px'>Edit</Button>
+  </Flex><Flex>
+  <HStack spacing='60px' justify='center'>
+      
+      <Box>
+        <Avatar name={userData.name} src={userData.profile_picture} width="100px" height="100px" />
+      </Box>
+      
+      <Box>
+        <Heading>{userData.name}</Heading>
+        <Text>{userData.email}</Text>
+        {userData.role && (
+        <Box as='button' borderRadius='md' bg='#bee8f0' color='gray' px={3} h={6}>
+          {userData.role}
+        </Box>
+      )} 
+    </Box> 
+
+    <Stack>
+        <Text>3</Text>
+        <Text>Posts</Text>
+      </Stack>
+      </HStack>
+      
+    </Flex></><Flex>
+      <Box>
+        <Heading>Photos</Heading>
+      </Box>
+    </Flex><Flex justifyContent='center'>
+
+    <Button
+        height='300px'
+        width='300px'
+        border='2px'
+        borderColor='blue.500'
+        borderRadius='0px'
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+      </svg>
+      </Button>
+      <Spacer />
+      <Box boxSize='sm'>
+        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+      </Box>
+      <Spacer />
+      <Box boxSize='sm'>
+        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+      </Box>
+      <Spacer />
+      <Box boxSize='sm'>
+        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+      </Box>
+    </Flex>
+    </Stack></>
+    }
