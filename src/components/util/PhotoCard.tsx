b27f56facc0bdbda07330f8319cda7300a117088
Sydorchuk Sofia <sydorchuk.pn@ucu.edu.ua>
2024-06-06 15:37:05 +0300

    Updated photo page styles

--- src/components/util/PhotoCard.tsx
+++ src/components/util/PhotoCard.tsx
@@ -1,46 +1,50 @@
-import { Box, Image, IconButton} from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 
+import { Box, Button, Image, Flex, Heading, Input, Stack, HStack, Spinner, Text, Avatar, RadioGroup, Radio, Wrap, WrapItem, Center, Spacer, InputLeftElement, InputGroup} from '@chakra-ui/react';
+import { useDocument } from 'react-firebase-hooks/firestore';
+// import { useNavigate } from 'react-router-dom';
 
 
 
-export const PhotoCard =(props: { userPhoto: any; photoAuthor: any; }) => {
-  const { userPhoto, photoAuthor } = props;
+export const PhotoCard =(props: { userData: any; userPhoto: any; photoAuthor: any; }) => {
+  const { userData, userPhoto, photoAuthor } = props;
   const { camera_model, description, location, photo_url, user } = userPhoto;
   const { email, role, name, profile_picture} = photoAuthor;
   const navigate = useNavigate();
-  console.log(camera_model, description, location, photo_url);
-  console.log(email, role, name, profile_picture);
-
-  return <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-
-    <IconButton 
-      icon={<ArrowBackIcon w={6} h={6} color='00232a' />}
-      w={2} h={10} borderRadius='100px' variant='outline' colorScheme='#f0f4f8'
-      bg="none"
-      onClick={() => { console.log('here'); navigate('/auth'); } } aria-label={''}/>
-
-  <Image src={photo_url} />
-
-  <Box p='6'>
-    <Box display='flex' alignItems='baseline'>
-      
-    <Box
-      mt='1'
-      fontWeight='semibold'
-      as='h4'
-      lineHeight='tight'
-      noOfLines={1}
-    >
-
+  // <Box fontWeight='semibold'>Categories: {userPhoto.categories ? userPhoto.categories.map((category: any) => <Box>{category}</Box>) : 'No categories'}</Box>
+  return (
+    <Box overflow='hidden' p='30px' backgroundColor="#f1f7f8" color="00232a">
+      <Button onClick={() => navigate('/') } fontSize='24px' fontWeight="bold" margin='10px' variant='ghost' borderRadius='100px' border='2px solid #005465' _hover={{ bg: '#C6EFF6' }}>←</Button>
+      <Flex alignItems='center' p='20px'>
+        <Avatar src={profile_picture} maxWidth="75px"/>
+        <Box ml='20px'>
+          <Heading size='md' color="#00232A" fontSize="24px">{name}</Heading>
+          <Text color='#005465'>{role}</Text>
+        </Box>
+      </Flex>
+      <Box p="10px" maxW={555}>
+        <Image src={photo_url} alt='image' />
+      </Box>
+      <Box p='10px' color="#005465" fontWeight="medium">
+        <Text>{description || 'Description (optional)'}</Text>
+        <Text>{location || 'Location (optional)'}</Text>
+        <Text>{camera_model || 'Camera (optional)'}</Text>
+      </Box>
+      <HStack spacing='10px' p='10px'>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='5px' fontWeight="medium" marginRight="5px">Traveling</Text>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='5px' fontWeight="medium">Nature</Text>
+      </HStack>
+      <HStack p='10px'>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='7px' fontWeight="medium" border='2px solid #005465' marginRight="5px">😊 13</Text>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='7px' fontWeight="medium" border='2px solid #005465' >❤️ 25</Text>
+      </HStack>
+      <Flex p='10px' alignItems='center'>
+        <Avatar src={userData.profile_picture} size='sm' />
+        <InputGroup ml='10px'>
+          <Input type='text' color='#00232a' placeholder='Write a comment...' bg='#C6EFF6' maxWidth="700px" height="50px"/>
+        </InputGroup>
+      </Flex>
     </Box>
-  </Box>
-  <Box fontWeight='semibold'>Description: {description ? description : 'No description'}</Box>
-  <Box fontWeight='semibold'>Location: {location ? location : 'No location'}</Box>
-  <Box fontWeight='semibold'>Camera Model: {camera_model ? camera_model : 'No camera model'}</Box>
-  <Box fontWeight='semibold'>Categories: {userPhoto.categories ? userPhoto.categories.map((category: any) => <Box>{category}</Box>) : 'No categories'}</Box>
-</Box>
-</Box>
-
-}
+  );
+};
