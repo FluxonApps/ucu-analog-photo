e849b3672a59f496af6f31d13cdd3cf4badafe6b
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-06-06 14:42:29 +0300

    not working vesion lol

--- src/components/PhotoPage.tsx
+++ src/components/PhotoPage.tsx
@@ -1,54 +1,41 @@
-import { Box, Button, Image, Flex, Heading, Input, Stack, HStack, Spinner, Text, Avatar, RadioGroup, Radio, Wrap, WrapItem, Center, Spacer, InputLeftElement, InputGroup} from '@chakra-ui/react';
-import { collection, doc } from 'firebase/firestore';
+import { Spinner } from '@chakra-ui/react';
+import { doc } from 'firebase/firestore';
 import { useDocument } from 'react-firebase-hooks/firestore';
 import { getAuth } from 'firebase/auth';
-
-import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useParams } from 'react-router-dom';
-
-import { db, storage } from '../../firebase.config';
-
-import { useEffect, useState } from 'react';
-import { ProfileCard } from './util/ProfileCard';
+import { db } from '../../firebase.config';
 import { PhotoCard } from './util/PhotoCard';
 
 const auth = getAuth();
 
-
 export function ProfilePage() {
-  const [user, userLoading] = useAuthState(auth);
-  const usersCollectionRef = collection(db, 'users');
-
-  const [docRef, setDocRef] = useState<any>(null);
-  
-  useEffect(() => {
-    if (user?.uid && (!docRef || docRef.id !== user.uid)) {
-      setDocRef(doc(usersCollectionRef, user.uid));
-    }
-  }, [user, usersCollectionRef]);
-  
-  const [userProfile, loadingUserProfile] = useDocument(docRef);
-
+  const [user, userLoading] = useAuthState(auth);  
   let { imageId } = useParams();
+  console.log('imageId:', imageId);
+  const [userPhoto, loadingUserPhoto] = useDocument(doc(db, 'userPhotos', imageId || 'abc'));
+  console.log('userPhoto data:', userPhoto?.data());
+  // console.log(userPhoto);
+  const [photoAuthor, loadingPhotoAuthor] = useDocument(doc(db, 'users', userPhoto?.data()?.user || 'abc'));
 
-  const [photoAuthor, loadingPhotoAuthor] = useDocument(doc(db, 'users', 'xQbvuayjyZNlasY4pekRHOeU2sE2'));
-  if (!imageId) {
+
+  if (!imageId && !userPhoto && !photoAuthor) {
     return <div>Error: No image ID provided</div>;
   }
-  const [userPhoto, loadingUserPhoto] = useDocument(doc(db, 'userPhotos', imageId));
-  console.log(photoAuthor);
+  // console.log(photoAuthor);
+
+
   // Do not show page content until auth state is fetched.
-  if (userLoading || loadingUserProfile || loadingUserPhoto || loadingPhotoAuthor || !userProfile || !userPhoto || !photoAuthor) {
+  if (userLoading || loadingUserPhoto || loadingPhotoAuthor || !userPhoto || !photoAuthor) {
     return <Spinner />;
   }
-
   // If user isn't signed in, redirect to auth page.
   if (!user) {
     return <Navigate to="/auth" replace />;
   }
 
 
-  return <PhotoCard userData={userProfile?.data()} userPhoto={userPhoto?.data()} photoAuthor={photoAuthor?.data()}></PhotoCard>
+  return <PhotoCard userPhoto={userPhoto?.data()} photoAuthor={photoAuthor?.data()}></PhotoCard>
 }
 
 export default ProfilePage;
--- src/components/util/PhotoCard.tsx
+++ src/components/util/PhotoCard.tsx
@@ -5,8 +5,8 @@ import { useDocument } from 'react-firebase-hooks/firestore';
 //
 
 
-export const PhotoCard =(props: { userData: any; userPhoto: any; photoAuthor: any; }) => {
-  const { userData, userPhoto, photoAuthor } = props;
+export const PhotoCard =(props: { userPhoto: any; photoAuthor: any; }) => {
+  const { userPhoto, photoAuthor } = props;
   const { camera_model, description, location, photo_url, user } = userPhoto;
   const { email, role, name, profile_picture} = photoAuthor;
   console.log(camera_model, description, location, photo_url);
