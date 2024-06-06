0a00bc0dfc1027d6fac9728e3f8f6306cd5d2092
Sydorchuk Sofia <sydorchuk.pn@ucu.edu.ua>
2024-06-06 17:25:35 +0300

    updated photolistpage

--- src/components/PhotoListPage.tsx
+++ src/components/PhotoListPage.tsx
@@ -30,7 +30,7 @@ const PhotoListComponent = (props: { userPhotos: any; setSearchValue: any} )=>{
   const {userPhotos, setSearchValue} = props
   console.log(userPhotos)
   if (userPhotos?.docs.length > 0) {
-    return <div> {userPhotos && userPhotos.docs.map((photo: any) => {
+    return <div>  {userPhotos && userPhotos.docs.map((photo: any) => {
       const navigate_photo = useNavigate();
 
       console.log(photo.data())
@@ -38,21 +38,19 @@ const PhotoListComponent = (props: { userPhotos: any; setSearchValue: any} )=>{
       photo.data().user
     );
     console.log(userProfile?.data())
-   
-   
     
     return (
       
-      <Flex flexDirection={'column'}>
+      <Flex flexDirection={'column'} pt="30px" pl="40px" color='#005465' bg='#F1F7F8'>
         
         <Flex flexDirection={'row'}>
-        <Box> <Avatar src={userProfile?.data().profile_picture} width='50px' height='50px'></Avatar> </Box>
-        <Box> {userProfile?.data().name ? userProfile?.data().name : 'User'}</Box> 
-        <Box> {userProfile?.data().role ? userProfile?.data().role : 'Roleless'}</Box> 
+        <Box mr="10px" mb="15px"> <Avatar src={userProfile?.data().profile_picture} width='50px' height='50px'></Avatar> </Box>
+        <Box> <Heading size='md' color="#00232A" fontSize="20px"> {userProfile?.data().name ? userProfile?.data().name : 'User'}</Heading>
+        <Text color='#005465' fontSize="16px">{userProfile?.data().role ? userProfile?.data().role : 'Roleless'}</Text></Box> 
 
         </Flex>
         <Flex flexDirection={'column'} onClick={()=> {navigate_photo('/photo/'+photo?.id)}}>
-        <img src={photo.data().photo_url}width='300px' height='300px'></img>
+        <Box mb='15px'><img src={photo.data().photo_url}width='400px' height='350px'></img></Box>
         <Box> {photo.data().description ? photo.data().description : ''}</Box>
         </Flex>
         <Flex flexDirection={'column'}>
@@ -62,12 +60,18 @@ const PhotoListComponent = (props: { userPhotos: any; setSearchValue: any} )=>{
         <Box>
         Camera: {photo.data().camera_model ? photo.data().camera_model : 'Unknown'}
         </Box>
-        <Box>
+        <HStack spacing='10px' mt="10px" mb='10px'>
           {console.log(photo.data().categories)}
-          Category: {photo.data().categories  ? photo.data().categories.join(', ') : 'None'}
-        </Box>
+          {photo.data().categories && photo.data().categories.map((category: any) => (<Text 
+          color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='5px' fontWeight="medium" marginRight="5px">
+          {category}</Text>))}
+        </HStack>
         </Flex>
-        <Flex flexDirection={'row'} gap={'5'}> <Button > Comments </Button><Button> Emoji </Button><Button> whateverelse </Button></Flex>
+        <Flex flexDirection={'row'} gap={'3'}> <Button bg='' _hover={{bg:'#C6EFF6'}}><Image src='src\components\util\add_comment.png' alt="Add comment" width="30px"></Image> </Button>
+        <HStack>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='7px' fontWeight="medium" border='2px solid #005465' marginRight="5px">😊 13</Text>
+        <Text color='#005465' bg='#C6EFF6' borderRadius='10px' px='15px' py='7px' fontWeight="medium" border='2px solid #005465' >❤️ 25</Text>
+      </HStack></Flex>
         </Flex>
        )
   })} </div>
@@ -84,7 +88,7 @@ const PhotoCard = (props: { userPhotos: any; setSearchValue: any}) =>  {
     // return <div> <PhotoListComponent userPhotos={userPhotos} setSearchValue={setSearchValue}/></div>
     // const navigate = useNavigate();
     const handleInput = (event:any) => setSearchValue(event.target.value)
-    return <div><Input onChange={handleInput} placeholder='Search'/>
+    return <div><Flex bg="#F1F7F8"><Input onChange={handleInput} placeholder='Search'  width="70vh" bg='#bee8f0' borderRadius='30px' mt="30px" ml="30px"/></Flex>
     <div> <PhotoListComponent userPhotos={userPhotos} setSearchValue={setSearchValue}/></div>
     </div>}
 
@@ -106,145 +110,6 @@ function PhotoListPage() {
     return <Navigate to="/auth" replace />;
   }
   return <PhotoCard setSearchValue = {setSearchValue} userPhotos={userPhotos}/>
-  
-  const [isEditing, setEditing] = useState(true);
-
-  const [newName, setNewName] = useState('');
-  const [newProfilePicture, setNewProfilePicture] = useState('');
-  const [newRole, setNewRole] = useState('');
-
-  const updateUser = async (id: string, newFields: any) => {
-    const userDoc = doc(db, 'users', id);
-    console.log(newFields);
-    await updateDoc(userDoc, newFields);
-  };
-
-  const addPhoto = async (id: string, photoUrl: string) => {
-    const userPhotosDoc = collection(db, 'users', id, 'photos');
-    await addDoc(userPhotosDoc, {photoUrl: photoUrl});
-  }
-
-  // Do not show page content until auth state is fetched.
-  if (userLoading) {
-    return <Spinner />;
-  }
-
-  // If user isn't signed in, redirect to auth page.
-  if (!user) {
-    return <Navigate to="/auth" replace />;
-  }
-
-  if (isEditing) {
-    return  <Box><Stack spacing="3">
-    <Input
-      onChange={async (event) => {
-        if (event.target.files && event.target.files.length > 0 && event.target.files[0] !== undefined) {
-          const imageRef = ref(storage, `profilePhotos/${user?.uid}`);
-          console.log(imageRef);
-          const imageSnapshot = await uploadBytes(imageRef, event.target.files[0]);
-          console.log(imageSnapshot);
-          const imageUrl = await getDownloadURL(imageSnapshot.ref);
-          console.log(imageUrl);
-          setNewProfilePicture(imageUrl);
-        }
-      }}
-      accept="image/png,image/jpeg"
-      type="file"
-      size="sm"
-    />
-    <Input
-      defaultValue={userProfile?.data()?.name}
-      onChange={(event) => {
-        setNewName(event.target.value);
-      }}
-      placeholder="Name..."
-      size="sm"
-    />
-    <Input
-      defaultValue={userProfile?.data()?.profile_picture}
-      onChange={(event) => {
-        setNewProfilePicture(event.target.value);
-      }}
-      placeholder="Write yours url image..."
-      size="sm"
-    />
-    <RadioGroup
-      defaultValue={userProfile?.data()?.role}
-      onChange={(value) => {
-        setNewRole(value);
-      }}
-    >
-      <Stack direction='row'>
-        <Radio value='Student'>Student</Radio>
-        <Radio value='Mentor'>Mentor</Radio>
-      </Stack>
-    </RadioGroup>
-  </Stack>
-  <Button width="50%" size="sm" colorScheme="green" onClick={() => {
-      // updateUser(user.uid, { name: newName, profile_picture: newProfilePicture, role: newRole });
-      setEditing(false);
-    }}>
-    Update User
-  </Button></Box>
-
-  } else {
-    return <><Stack justify="spaceAround" p={20}><>
-    <Wrap>
-    <Input placeholder='Search your photo' width="70vh"/>
-    <Spacer />
-    <Box>
-    <Avatar name={userProfile?.data()?.name} src={userProfile?.data()?.profile_picture} width="50px" height="50px" />
-    </Box></Wrap>
-    
-    
-    <Flex>
-        <Box>
-          <Heading>Personal info</Heading>
-        </Box>
-        <Spacer />
-        <Button onClick={() => setEditing(true)}>Edit</Button>
-      </Flex><Flex>
-      <HStack spacing='60px' justify='center'>
-          
-          <Box>
-            <Avatar name={userProfile?.data()?.name} src={userProfile?.data()?.profile_picture} width="100px" height="100px" />
-          </Box>
-          
-          <Box>
-            <Heading>{userProfile?.data()?.name}</Heading>
-            <Text>{userProfile?.data()?.email}</Text>
-            {userProfile?.data()?.role && (
-            <Box as='button' borderRadius='md' bg='green' color='white' px={3} h={6}>
-              {userProfile?.data()?.role}
-            </Box>
-          )} 
-        </Box> 
-
-        <Stack>
-            <Text>3</Text>
-            <Text>Posts</Text>
-          </Stack>
-          </HStack>
-          
-        </Flex></><Flex>
-          <Box>
-            <Heading>Photos</Heading>
-          </Box>
-        </Flex><Flex justifyContent='center'>
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-          <Spacer />
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-          <Spacer />
-          <Box boxSize='sm'>
-            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-          </Box>
-        </Flex>
-        </Stack></>
-  }
 }
 
 export default PhotoListPage;
