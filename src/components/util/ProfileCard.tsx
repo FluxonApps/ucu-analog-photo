a7d84f9f88a2e4d57549e23b660c4f07a46b2207
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-06-06 11:17:00 +0300

    add cotegories and redirection to add_photo page

--- src/components/util/ProfileCard.tsx
+++ src/components/util/ProfileCard.tsx
@@ -1,9 +1,10 @@
 import { Box, Button, Image, Flex, Heading, Input, Stack, HStack, Spinner, Text, Avatar, RadioGroup, Radio, Wrap, WrapItem, Center, Spacer, InputLeftElement, InputGroup} from '@chakra-ui/react';
-
+import { useNavigate } from 'react-router-dom';
 
 export const ProfileCard =(props: { userData: any; setEditing: any; }) => {
     console.log(props)
 const { userData, setEditing } = props;
+const navigate = useNavigate();
 return <><Stack justify="spaceAround" px={40} py={10} spacing='15' backgroundColor='#f1f7f8' minHeight='100vh' color='00232a'><>
 <Wrap>
 <Input placeholder='Search your photo' width="70vh" bg='#bee8f0' borderRadius='30px'/>
@@ -54,6 +55,7 @@ return <><Stack justify="spaceAround" px={40} py={10} spacing='15' backgroundCol
         border='2px'
         borderColor='blue.500'
         borderRadius='0px'
+        onClick={() => navigate('/add_photo')}
       >
       <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
